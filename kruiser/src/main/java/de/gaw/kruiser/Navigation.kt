02ae3b21a6e1858167d50119875c87cd97957576
Alexander Steenbergen <alexandersteenbergen@gmail.com>
2023-08-08 16:04:32 +0200

    Ordering

--- kruiser/src/main/java/de/gaw/kruiser/Navigation.kt
+++ kruiser/src/main/java/de/gaw/kruiser/Navigation.kt
@@ -5,12 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.zIndex
 import de.gaw.kruiser.android.LocalNavigationState
-import de.gaw.kruiser.destination.Destination
+import de.gaw.kruiser.destination.Render
 import de.gaw.kruiser.state.NavigationState
 import de.gaw.kruiser.state.collectCurrentStack
 import de.gaw.kruiser.state.collectIsEmpty
@@ -18,14 +16,6 @@ import de.gaw.kruiser.state.pop
 import de.gaw.kruiser.transition.LocalExitTransitionTracker
 import de.gaw.kruiser.transition.collectCurrentExitTransition
 import de.gaw.kruiser.transition.rememberExitTransitionTracker
-
-@Composable
-fun Destination.Render() {
-    key(this) {
-        val screen = remember(this) { build() }
-        screen.Content()
-    }
-}
 
 @Composable
 fun AnimatedNavigation(
--- kruiser/src/main/java/de/gaw/kruiser/destination/Destination.kt
+++ kruiser/src/main/java/de/gaw/kruiser/destination/Destination.kt
@@ -1,8 +1,19 @@
 package de.gaw.kruiser.destination
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import de.gaw.kruiser.screen.Screen
 import java.io.Serializable
 
 interface Destination : Serializable {
     fun build(): Screen
+}
+
+@Composable
+fun Destination.Render() {
+    key(this) {
+        val screen = remember(this) { build() }
+        screen.Content()
+    }
 }
