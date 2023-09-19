f7fb65154d0eeb2df8dc514b9613e7b7e85b11d6
Alexander Steenbergen <alexandersteenbergen@gmail.com>
2023-09-19 15:45:37 +0200

    Refactoring

--- sample/src/main/java/de/gaw/kruiser/sample/samples/DashboardDestination.kt
+++ sample/src/main/java/de/gaw/kruiser/sample/samples/DashboardDestination.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import de.gaw.kruiser.destination.Destination
 import de.gaw.kruiser.sample.samples.push.PushAndPopDestinationDefault
+import de.gaw.kruiser.sample.theme.KruiserPreviewTheme
+import de.gaw.kruiser.sample.transition.HorizontalCardStackTransition
 import de.gaw.kruiser.screen.Screen
 import de.gaw.kruiser.screen.ScreenModel
 import de.gaw.kruiser.service.ScopedServiceProvider
 import de.gaw.kruiser.service.ScopedServiceProvider.ServiceFactory
 import de.gaw.kruiser.service.scopedService
+import de.gaw.kruiser.state.InMemoryNavigationState
 import de.gaw.kruiser.state.NavigationState
 import de.gaw.kruiser.state.push
 
@@ -26,12 +31,36 @@ private class DashboardScreen : Screen {
     override val destination: Destination = DashboardDestination
 
     @Composable
-    override fun Content() {
+    override fun Content() = HorizontalCardStackTransition {
         val model = scopedService(DashboardScreenModelFactory)
+        Dashboard(
+            model = model,
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+}
+
+@Composable
+private fun Dashboard(
+    model: DashboardModel,
+    modifier: Modifier = Modifier,
+) {
+    Dashboard(
+        modifier = modifier,
+        items = model.menuItems,
+    )
+}
+
+@Composable
+private fun Dashboard(
+    items: List<Pair<String, () -> Unit>>,
+    modifier: Modifier = Modifier,
+) {
+    Surface {
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = modifier,
         ) {
-            items(model.menuItems) { (title, onClick) ->
+            items(items) { (title, onClick) ->
                 ListItem(
                     modifier = Modifier.clickable(onClick = onClick),
                     headlineContent = {
@@ -45,7 +74,16 @@ private class DashboardScreen : Screen {
     }
 }
 
-private class DashboardScreenModel(
+@Preview
+@Composable
+private fun DashboardPreview() = KruiserPreviewTheme {
+    Dashboard(
+        model = DashboardModel(navigationState = InMemoryNavigationState()),
+        modifier = Modifier.fillMaxSize(),
+    )
+}
+
+private class DashboardModel(
     private val navigationState: NavigationState,
 ) : ScreenModel {
 
@@ -58,7 +96,7 @@ private class DashboardScreenModel(
     }
 }
 
-private object DashboardScreenModelFactory : ServiceFactory<DashboardScreenModel> {
-    override fun ScopedServiceProvider.ServiceContext.create(): DashboardScreenModel =
-        DashboardScreenModel(navigationState)
+private object DashboardScreenModelFactory : ServiceFactory<DashboardModel> {
+    override fun ScopedServiceProvider.ServiceContext.create(): DashboardModel =
+        DashboardModel(navigationState)
 }
