6e6ad656f786282770daa21c89ee4f2735e31dc3
Alexander Steenbergen <alexandersteenbergen@gmail.com>
2024-03-24 10:05:46 +0100

    Version bumps, lower min sdk of 23

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "de.gaw.kruiser"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
